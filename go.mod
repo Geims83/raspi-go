f47d339cf53b0686f074a8c27676ca896fb80893
Geims83 <yasu83@gmail.com>
2022-12-29 13:44:26 +0100

    iot module

--- go.mod
+++ go.mod
@@ -5,5 +5,8 @@ go 1.18
 require (
 	github.com/MichaelS11/go-dht v0.1.0 // indirect
 	github.com/amenzhinsky/iothub v0.9.0 // indirect
+	github.com/eclipse/paho.mqtt.golang v1.3.5 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/net v0.0.0-20211020060615-d418f374d309 // indirect
 	periph.io/x/periph v3.6.8+incompatible // indirect
 )
--- main.go
+++ main.go
@@ -3,23 +3,81 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
+	// "net/http"
+	"time"
+
+	"context"
+	"os"
+	"os/signal"
+    "syscall"
+	"github.com/amenzhinsky/iothub/iotdevice"
+	iotmqtt "github.com/amenzhinsky/iothub/iotdevice/transport/mqtt"
 )
 
-func read(w http.ResponseWriter, req *http.Request) {
+// func read(w http.ResponseWriter, req *http.Request) {
 
-	status, err := readData()
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		fmt.Fprintf(w, "Temperatura: %v\n", status.Temperature)
-		fmt.Fprintf(w, "Umidità: %v\n", status.Humidity)
-	}
-}
+// 	status, err := readData()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	} else {
+// 		fmt.Fprintf(w, "Temperatura: %v\n", status.Temperature)
+// 		fmt.Fprintf(w, "Umidità: %v\n", status.Humidity)
+// 	}
+// }
+
+// func main_web() {
+
+// 	http.HandleFunc("/", read)
+
+// 	http.ListenAndServe(":8090", nil)
+// }
 
 func main() {
 
-	http.HandleFunc("/", read)
+	client, err := iotdevice.NewFromConnectionString(
+		iotmqtt.New(), os.Getenv("IOTHUB_DEVICE_CONNECTION_STRING"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	// connect to the iothub
+	if err = client.Connect(context.Background()); err != nil {
+		log.Fatal(err)
+	}
 
-	http.ListenAndServe(":8090", nil)
+	ticker := time.NewTicker(5 * time.Second)
+    done := make(chan bool)
+    
+	go func() {
+        for {
+            select {
+            case <-done:
+                return
+            case <-ticker.C:
+				status, err := readData()
+
+				if err != nil {
+					log.Fatal(err)
+				} else {
+					
+					// if err = h.SendEvent(context.Background(), []byte(`hello`)); err != nil {
+					// 	log.Fatal(err)
+					// }
+					fmt.Printf("Temperatura: %v\n", status.Temperature)
+					fmt.Printf("Umidità: %v\n", status.Humidity)
+				}
+            }
+        }
+    }()
+
+	c := make(chan os.Signal)
+    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+    go func() {
+        <-c
+		ticker.Stop()
+		done <- true
+		fmt.Println("Ticker stopped")
+    }()
+
 }
